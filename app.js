e6edfddfb1a3da9cc66436fdaf06857bf1457f25
UzomaAkpala <francisakpala@yahoo.com>
2023-11-09 12:26:39 +0100

    reading data from html files and on browser

---
+++ app.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 4000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("page/home.html", "utf8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("page/about.html", "utf8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    fs.readFile("page/PageNotFound.html", "utf8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+console.log(`server is running at http://localhost:${PORT}`);
+server.listen(PORT);
