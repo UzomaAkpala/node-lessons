2bbdeca1f97a47da541f8c7a42579eac79f23d64
UzomaAkpala <francisakpala@yahoo.com>
2023-11-10 16:57:55 +0100

    http module basics

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 const http = require("http");
-const fs = require("fs");
-const PORT = 4000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile("page/home.html", "utf8", (err, data) => {
-      if (err) throw err;
-      res.write(data);
-      res.end();
-    });
+    res.end("Welcome to our home page");
   } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile("page/about.html", "utf8", (err, data) => {
-      if (err) throw err;
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === "/create-file") {
-    const data = "<h1>This is test file</h1>";
-    res.writeHead(200, { "Content-Type": "text/html" });
-    fs.writeFile("temp/test.html", data, (err) => {
-      if (err) throw err;
-      res.write("<h1>file is created</h1>");
-      res.end();
-    });
+    res.end("Here is our short history");
   } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    fs.readFile("page/PageNotFound.html", "utf8", (err, data) => {
-      if (err) throw err;
-      res.write(data);
-      res.end();
-    });
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+    `);
   }
 });
 
-console.log(`server is running at http://localhost:${PORT}`);
-server.listen(PORT);
+server.listen(5000);
