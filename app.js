bd64654fb0c22242f396c6e06e395e812bb6d127
UzomaAkpala <francisakpala@yahoo.com>
2023-11-11 14:12:30 +0100

    event emitter basic setup

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const http = require("http");
+const EventEmitter = require("events");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Welcome to our home page");
-  } else if (req.url === "/about") {
-    res.end("Here is our short history");
-  } else {
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `);
-  }
+const customEmitter = new EventEmitter();
+
+customEmitter.on("response", () => {
+  console.log(`data received`);
 });
 
-server.listen(5000);
+customEmitter.emit("response");
